a8e58983683a7e49174c3496f556cb3a973861e4
iNeso1984 <vanessawithunweb@gmail.com>
2025-01-23 18:19:59 -0600

    Fix configuration error

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
 import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte'; // Fix the import here
 
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    rollupOptions: {
-      external: ['chart.js'],  // This will tell Rollup to treat chart.js as an external dependency
-    },
-  },
 });
-
-
